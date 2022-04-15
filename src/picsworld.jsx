b8871212784ae6f3b9d140078b5d147dff568a5d
princemakadiya <72595181+princemakadiya@users.noreply.github.com>
2022-04-15 13:09:18 +0530

    Create picsworld.jsx

---
+++ src/picsworld.jsx
@@ -0,0 +1,26 @@
+// import React, { useState } from "react";
+// import Sresult from "./picsworldresult";
+
+// const Search =()=>{
+
+//     const[img,setimg]=useState("");
+    
+//     const inputEvent =(event)=>{
+//         const data = event.target.value;
+//         setimg(data);
+//         console.log(data)
+
+//     } 
+
+//     return(
+//         <>
+//         <h1>Welcome to <span id="heading" >PicsWorld</span> </h1>
+//         <div className="searchbar">
+//         <input type={"text"} placeholder={"search anything"} onChange={inputEvent} value={img}/>    
+//        { img ==="" ? null :<Sresult names={img}/>} 
+//         </div>    
+//         </>
+//     )
+// };
+
+// export default Search;
