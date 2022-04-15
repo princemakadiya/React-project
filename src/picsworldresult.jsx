9eef60045362aa36c1d06c0982a94a1b46dfab80
princemakadiya <72595181+princemakadiya@users.noreply.github.com>
2022-04-15 13:09:21 +0530

    Create picsworldresult.jsx

---
+++ src/picsworldresult.jsx
@@ -0,0 +1,69 @@
+// import React from "react";
+// const Sresult = (props) => {
+//   const img = ` https://source.unsplash.com/600x400/?${props.names}`;
+//   const img1 = ` https://source.unsplash.com/601x400/?${props.names}`;
+//   const img2 = ` https://source.unsplash.com/602x400/?${props.names}`;
+//   const img3 = ` https://source.unsplash.com/603x400/?${props.names}`;
+//   const img4 = ` https://source.unsplash.com/604x400/?${props.names}`;
+//   const img5 = ` https://source.unsplash.com/605x400/?${props.names}`;
+//   const img6 = ` https://source.unsplash.com/606x400/?${props.names}`;
+//   const img7 = ` https://source.unsplash.com/607x400/?${props.names}`;
+//   const img8= ` https://source.unsplash.com/608x400/?${props.names}`;
+//   const img9 = ` https://source.unsplash.com/609x400/?${props.names}`;
+//   const img10 = ` https://source.unsplash.com/611x400/?${props.names}`;
+//   const img11 = ` https://source.unsplash.com/611x400/?${props.names}`;
+//   const img12 = ` https://source.unsplash.com/612x400/?${props.names}`;
+//   const img13 = ` https://source.unsplash.com/613x400/?${props.names}`;
+//   const img14 = ` https://source.unsplash.com/614x400/?${props.names}`;
+//   const img15 = ` https://source.unsplash.com/615x400/?${props.names}`;
+//   const img16 = ` https://source.unsplash.com/616x400/?${props.names}`;
+//   const img17 = ` https://source.unsplash.com/617x400/?${props.names}`;
+//   const img18 = ` https://source.unsplash.com/610x400/?${props.names}`;
+//   const img19 = ` https://source.unsplash.com/611x400/?${props.names}`;
+//   const img20 = ` https://source.unsplash.com/612x400/?${props.names}`;
+//   const img21 = ` https://source.unsplash.com/613x400/?${props.names}`;
+//   const img22 = ` https://source.unsplash.com/614x400/?${props.names}`;
+//   const img23= ` https://source.unsplash.com/620x400/?${props.names}`;
+//   const img24 = ` https://source.unsplash.com/626x400/?${props.names}`;
+//   const img25= ` https://source.unsplash.com/627x400/?${props.names}`;
+
+//   return (
+               
+//     <>
+//       <div className="images">
+//         <img src={img} alt="searching..."/>
+//         <img src={img1} alt="searching..."/>        
+//         <img src={img2} alt="searching..."/>
+//         <img src={img3} alt="searching..."/>
+//         <img src={img4} alt="searching..."/>
+//         <img src={img5} alt="searching..."/>
+//         <img src={img6} alt="searching..."/>
+//         <img src={img7} alt="searching..."/>
+        
+//         <img src={img8} alt="searching..."/>
+//         <img src={img9} alt="searching..."/>
+//         <img src={img10} alt="searching..."/>
+//         <img src={img11} alt="searching..."/>        
+//         <img src={img12} alt="searching..."/>
+//         <img src={img13} alt="searching..."/>
+//         <img src={img14} alt="searching..."/>
+//         <img src={img15} alt="searching..."/>
+//         <img src={img16} alt="searching..."/>
+//         <img src={img17} alt="searching..."/>
+        
+//         <img src={img18} alt="searching..."/>
+//         <img src={img19} alt="searching..."/>        
+//         <img src={img20} alt="searching..."/>
+//         <img src={img21} alt="searching..."/>
+//         <img src={img22} alt="searching..."/>
+//         <img src={img23} alt="searching..."/>
+//         <img src={img24} alt="searching..."/>
+//         <img src={img25} alt="searching..."/>
+
+
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Sresult;
