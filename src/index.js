d49057285cf376b127e2af9fe47359299dc174ef
princemakadiya <72595181+princemakadiya@users.noreply.github.com>
2022-04-15 13:09:01 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App"
+import Statwise from "./statwise"
+ReactDOM.render(<App/>, document.getElementById("root"));
