b6309e2f6b27a77d4f9c8cdfdd5a0fb4678f7167
princemakadiya <72595181+princemakadiya@users.noreply.github.com>
2022-04-18 17:56:21 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App"
-import Statwise from "./statwise"
+import Statwise from "./dashboard"
 ReactDOM.render(<App/>, document.getElementById("root"));
